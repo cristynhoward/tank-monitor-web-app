0c671c24805df5ac150e8b69d0bac46364459bd4
Cristyn Howard <cristyn.howard@mail.utoronto.ca>
2020-04-21 01:03:27 -0400

    Add basic config file.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('./config.js')();
 
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
@@ -20,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/login', loginRouter);
+app.use(config.urls.login, loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ config.js
@@ -0,0 +1,11 @@
+
+module.exports = function() {
+    this.config = {
+    	app: {
+    		port: 3000
+    	},
+    	urls: {
+    		login: '/login'
+    	}
+    }
+}
